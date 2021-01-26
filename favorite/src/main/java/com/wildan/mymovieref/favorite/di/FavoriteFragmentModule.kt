672b783f6acaa6f2895a991b0888dd7d6c51e7ce
muhammad wildan iskandar <muhammadwildaniskandar@gmail.com>
2021-01-26 18:29:44 +0800

    add fragment module

--- favorite/src/main/java/com/wildan/mymovieref/favorite/di/FavoriteFragmentModule.kt
+++ favorite/src/main/java/com/wildan/mymovieref/favorite/di/FavoriteFragmentModule.kt
@@ -3,17 +3,17 @@ package com.wildan.mymovieref.favorite.di
 import com.wildan.mymovieref.core.data.local.FavoriteDao
 import com.wildan.mymovieref.core.data.remote.TheMovieDBAPI
 import com.wildan.mymovieref.core.data.repository.PopularRepository
-import com.wildan.mymovieref.core.di.ActivityViewModelModule
+import com.wildan.mymovieref.core.di.FragmentViewModelModule
 import com.wildan.mymovieref.core.domain.repository.IPopularRepository
 import com.wildan.mymovieref.favorite.ui.FavoriteViewModel_HiltModule
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.FragmentComponent
 
-@Module(includes = [com.wildan.mymovieref.favorite.ui.FavoriteViewModel_HiltModule::class, ActivityViewModelModule::class])
-@InstallIn(ActivityComponent::class)
-abstract class FavoriteActivityModule {
+@Module(includes = [ com.wildan.mymovieref.favorite.ui.FavoriteViewModel_HiltModule::class, FragmentViewModelModule::class])
+@InstallIn(FragmentComponent::class)
+abstract class FavoriteFragmentModule {
     companion object {
         @Provides
         fun providePopularRepository(
